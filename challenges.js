ee08815a34b496fc9a3431d43e86f811063da07c
Aaron <aaronpfink@gmail.com>
2021-04-08 17:17:49 -0400

    Create challenges.js

---
+++ challenges.js
@@ -0,0 +1,67 @@
+// Level 1
+
+let dog1 = {
+    name: "Daniel",
+    breed: "Spaniel",
+    favoriteActivity: "go on walks",
+    weight: 29,
+    age: 8
+}
+let dog2 = {
+    name: "Scraps",
+    breed: "German Shepherd",
+    favoriteActivity: "roll over",
+    weight: 63,
+    age: 6
+}
+
+// Two dogs have been defined for you above. Print out their names in the following sentence: My two dogs are named ___ and ___.
+
+
+
+// For each dog, print out a sentence of the form: Olive the labradoodle weighs 36 pounds and loves to run up and down the stairs.
+
+
+
+// Use an if-statement to print out which dog is heavier, and another to print out which is older.
+
+
+
+// Try changing their weights and ages at the top to make sure your if-statements work how you expect.
+
+
+
+// Level 2
+
+// Define three objects representing shoes. Include their color, their size, and an attribute (comftorable, sparkly, etc.).
+
+
+
+// For each shoe, print out a sentence of the form: The purple slipper is fuzzy.
+
+
+
+// Use conditionals to print out which shoe is the largest.
+// Hint: You'll need more than one simple if-else.
+
+
+
+// Level 3
+
+// Define two objects representing people. Include their name, their favorite color, and their shoe size.
+
+
+
+// For each person, print out a sentence of the form: "Faith loves the color green and wears a size 9 shoe.
+
+
+
+// Save a person and a shoe in these two variables:
+// let person = ___
+// let shoe = ___
+
+// Then write some code that tells you if the person can wear the shoe.
+
+
+
+// Change which person or shoe you assign to those variables above and make sure your code still works.
